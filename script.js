a9aa14dbe0d4af9a5d72dff171a8efd0d6ea2305
Margarida Melo <mmarg.melo@gmail.com>
2021-12-26 23:36:03 +0000

    light/dark mode button

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints a message in the browser's dev tools console
-console.log("Hello, World!");
+// this one is jut to wait for the page to load
+document.addEventListener('DOMContentLoaded', () => {
+    const themeStylesheet = document.getElementById('theme')
+    const themeToggle = document.getElementById('theme-toggle')
+    themeToggle.addEventListener('click', () => {
+        // if it's light -> go dark
+        if (themeStylesheet.href.includes('light')) {
+            themeStylesheet.href = 'dark-theme.css'
+            themeToggle.innerText = 'Light mode'
+        } else {
+            // if it's dark -> go light
+            themeStylesheet.href = 'light-theme.css'
+            themeToggle.innerText = 'Dark mode'
+        }
+    })
+})
